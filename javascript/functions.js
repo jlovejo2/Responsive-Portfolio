e11d74d7e9be24287a83bdebe53fbfb069518e14
James Lovejoy <james.lovejoy2@gmail.com>
2019-12-18 21:02:21 -0600

    added function to generate portfolio links, put them in cards as well, updated images, added a hover event to nav-links

--- javascript/functions.js
+++ javascript/functions.js
@@ -3,46 +3,89 @@ function init() {
 
     $(".content").addClass("animated fadeInRight");
 
+    //code to be used if I want to delay the animation
+    // setTimeout(() => {
+
+    //     $(".content").addClass("animated fadeInRight");
+
+    // }, 1000 * 2);
+
+    $(".nav-link").hover(function(){
+    
+        $(this).toggleClass("animated bounce")
+    
+    })
+
 }
 
 
-function createCarousel (mainDiv, imgSRC, numOfImg) {
+function createCarousel(mainDiv, imgSRC, numOfImg) {
 
-    var carouselColDiv = $("<div>").attr({"id":"carousel","class":"carousel slide text-center","data-ride":"carousel"});
-    var carouselIndicators = $("<ul>").attr("class","carousel-indicators");
-    var carouselInnerDiv = $("<div>").attr("class","carousel-inner");
-    
-    carouselIndicators.append($("<li>").attr({"data-target":"#carousel","data-slide-to":"1"}));
-    carouselIndicators.append($("<li>").attr({"data-target":"#carousel","data-slide-to":"2"}));
-    carouselIndicators.prepend($("<li>").attr({"class":"active","data-target":"carousel","data-slide-to":"0"}));
-    carouselColDiv.append(carouselIndicators);
+    var carouselInnerDiv = $("#carouselInner")
 
-    for (i=1; i <= numOfImg; i++){
-        
+    for (i = 1; i <= numOfImg; i++) {
+
         var imgLink = imgSRC + "img_000" + i + ".jpg";
-        var img = $("<img>").attr({"class":"img-fluid rounded","src":imgLink,"alt":"Photo"});
+        var img = $("<img>").attr({ "class": "img-fluid rounded", "src": imgLink, "alt": "Photo" });
 
-        carouselInnerDiv.append(($("<div>").attr("class","carousel-item")).append(img)); 
+        carouselInnerDiv.append(($("<div>").attr("class", "carousel-item")).append(img));
     }
 
-    carouselColDiv.append(carouselInnerDiv);
-
     carouselInnerDiv.children().first().addClass("active");
-
-    var prevSlideIndicator = $("<a>").attr({"class":"carousel-control-prev","href":"#carousel","data-slide":"prev"});
-    var nextSlideIndicator = $("<a>").attr({"class":"carousel-control-next","href":"#carousel","data-slide":"next"});
-
-    prevSlideIndicator.append($("<span>").attr("class","carousel-control-prev-icon"));
-    nextSlideIndicator.append($("<span>").attr("class","carousel-control-next-icon"));
-    
-    carouselColDiv.append(prevSlideIndicator);
-    carouselColDiv.append(nextSlideIndicator);
-
-    mainDiv.append($("<div>").attr("class","col-md-1 col-lg-1"));
-    mainDiv.append($("<div>").attr("class"," col-md-10 col-lg-10").append(carouselColDiv));
-    mainDiv.append($("<div>").attr("class","col-md-1 col-lg-1"));
 }
 
-function createPortfolioCard () {
-    
-}
+function createPortfolioCard(objOfImgSRCLinks) {
+
+    var rowDiv = $("<div>").attr({ "class": "row mr-2 p-2" });
+    var count = 0;
+    console.log(objOfImgSRCLinks);
+
+    $.each(objOfImgSRCLinks, function (key, value) {
+
+        console.log(key);
+        console.log(value[0]);
+
+        if (count < 3) {
+
+            var colDiv = $("<div>").attr({ "class": "col-xl-4 col-lg-6 col-md-6 col-md-6 p-2" });
+            var cardDiv = $("<div>").attr({"class":"card text-center"});
+            var cardBodyDiv = $("<div>").attr({"class":"card-body shadow-lg p-2"});
+            var cardTitle = $("<h5>").attr({"class":"card-title p-1 m-2","style":"border-bottom: solid 3px rgba(0,0,0,.5)"});
+            var imgDiv = $("<img>").attr({ "class": "card-img-top", "src": key });
+            var aDiv = $("<a>").attr({"href": value[0], "class": "btn btn-info portfolio_button m-2" });
+
+            cardTitle.text(value[1]);
+            aDiv.text("Click here to explore!")
+
+            rowDiv.append(colDiv.append(cardDiv.append(cardBodyDiv.append(cardTitle, imgDiv, aDiv))));
+
+            count++;
+        } else {
+            $("#portfolioContent").append(rowDiv);
+            rowDiv = $("<div>").attr({ "class": "row mr-2 p-2" });
+            count = 0;
+        }
+
+            $("#portfolioContent").append(rowDiv);
+            count= 0;
+
+    });
+};
+
+
+
+
+    // <div class="row mr-2 p-2">
+    //         <div class="col-xl-4 col-lg-6 col-md-6 col-md-6 p-2">
+    //             <img class="img-responsive" src="assets/images/IMG_9209.JPG">
+    //                 <a class="portfolio-link" href="#" >Trivia Game</a>
+    //         </div>
+    //         <div class="col-xl-4 col-lg-6 col-md-6 p-2">
+    //           <img class="img-responsive" src="assets/images/IMG_9209.JPG">
+    //             <a class="portfolio-link" href="#" >Trivia Game</a>
+    //         </div>
+    //         <div class="text-center col-xl-4 p-2">
+    //           <img class="img-responsive" src="assets/images/IMG_9209.JPG">
+    //             <a class="portfolio-link" href="#" >Trivia Game</a>
+    //         </div> 
+    //     </div>
--- javascript/javascript-index.js
+++ javascript/javascript-index.js
@@ -4,17 +4,6 @@ var imgSRCLink = "assets/images/Carousel/"
 
 init()
 
-// var carouselDiv = $("<div>").attr({"class":"carousel slide","dara-ride":"carousel"})
-
-
-
-
-
-$(".nav-link").hover(function(){
-    
-    $(this).toggleClass("animated bounce")
-
-})
 
 createCarousel($("#carouselRow"),imgSRCLink, 5);
 
