72eedce6016b3c716a46f0278e3c826aaf19d662
James Lovejoy <james.lovejoy2@gmail.com>
2019-12-18 21:03:01 -0600

    created javascript file specific to portfolio html

---
+++ javascript/javascriptPortfolio.js
@@ -0,0 +1,16 @@
+var portfolioObject = {
+
+    "assets/images/password.jpg" : ["https://jlovejo2.github.io/Password_Generator/","Password Generator"],
+    "assets/images/trivia_quiz.jpg" : ["https://jlovejo2.github.io/Trivia_quiz/", "Trivia Quiz"],
+    "assets/images/weather_image.png" : ["https://jlovejo2.github.io/Weather_Dashboard/", "Weather Dashboard"],
+    "assets/images/calendar.jpg" : ["https://jlovejo2.github.io/Word-Day-Calendar/", "Work Day Calendar"]
+
+};
+
+$(document).ready(function(){
+
+    init();
+
+    createPortfolioCard(portfolioObject);  
+
+});
